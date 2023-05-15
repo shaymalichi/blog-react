f5c543774289cd8176e760ab3810b6f6c5d15c8f
ShayMalichi <46198407+ShayMalichi@users.noreply.github.com>
2023-05-15 19:02:18 +0300

    changes

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import {Button, TextField} from "@mui/material";
-
+import {Button, TextField, Grid} from "@mui/material";
 
 function LoginForm() {
     return (
-        <form>
-            <div>
-                <TextField id="outlined-basic" label="Username" variant="outlined" />
-            </div>
-            <div>
-                <TextField id="outlined-basic" label="password" variant="outlined" />
-            </div>
-            <Button variant="contained">Login</Button>
-        </form>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
+        >
+            <form>
+                <h2>Login</h2>
+                <div>
+                    <TextField id="outlined-basic" label="Username" variant="outlined" />
+                </div>
+                <div>
+                    <TextField id="outlined-basic" label="password" variant="outlined" />
+                </div>
+                <Button variant="contained">Login</Button>
+            </form>
+        </Grid>
     );
 }
 
--- src/NewPost.js
+++ src/NewPost.js
@@ -1,21 +1,34 @@
-import {Button, TextField} from "@mui/material";
+import {Button, TextField, Grid} from "@mui/material";
 import React from "react";
 
 const NewPost = () => {
   return (
-      <div>
-        <h2>New Post</h2>
-          <TextField
-              id="outlined-multiline-static"
-              label="Enter Blog Post.."
-              multiline
-              rows={4}
-              defaultValue="Default Value"
-          />
-        <br />
-          <Button variant="contained">Send</Button>
-      </div>
+
+      <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: '100vh' }}
+      >
+          <div>
+              <h2>New Post</h2>
+              <TextField
+                  id="outlined-multiline-static"
+                  label="Enter Blog Post.."
+                  multiline
+                  rows={4}
+                  defaultValue="Default Value"
+              />
+              <br />
+              <Button variant="contained">Send</Button>
+          </div>
+      </Grid>
   );
 };
 
 export default NewPost;
+
+
+
