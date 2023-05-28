6ad3a9b96f30b267ffb3ed5be73d0c7d34bea4cb
ShayMalichi <46198407+ShayMalichi@users.noreply.github.com>
2023-05-28 14:04:47 +0300

    backend

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import mysql.connector as mysql
 import json
 from flask_cors import CORS
 from datetime import datetime
+from flask import jsonify
+
 
 
 db = mysql.connect(
@@ -61,17 +63,20 @@ def get_all_posts():
 # 	header = ['id', 'name', 'population']
 # 	return json.dumps(dict(zip(header, record)))
 
+@app.route('/new-post', methods=['POST'])
 def add_city():
-	data = request.get_json()
-	print(data)
-	query = "INSERT INTO posts (user_id, id, title, body, created_at) VALUES (1, 'random_id', 'Random Title', 'Random Body', NOW())"
-	values = (data['user_id'], data['id'], data['title'], data['body'], data['created_at'])
-	cursor = db.cursor()
-	cursor.execute(query, values)
-	db.commit()
-	new_city_id = cursor.lastrowid
-	cursor.close()
-	#return get_city(new_city_id)
+    data = request.get_json()
+    query = "INSERT INTO posts (user_id, id, title, body, created_at) VALUES (%s, %s, %s, %s, %s)"
+    values = (data['user_id'], data['id'], data['title'], data['body'], data['created_at'])
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    db.commit()
+    new_city_id = cursor.lastrowid
+    cursor.close()
+
+    # Return a response indicating the success of the operation
+    return jsonify({'message': 'Post added successfully', 'new_city_id': new_city_id})
+
 
 if __name__ == "__main__":
 	app.run()
--- src/dataComponenets/NewPost.js
+++ src/dataComponenets/NewPost.js
@@ -1,6 +1,7 @@
-import {Button, TextField, Grid, createTheme, ThemeProvider} from "@mui/material";
-import React from "react";
-import {orange} from "@mui/material/colors";
+import React, { useEffect, useState } from "react";
+import { Button, TextField, Grid, createTheme, ThemeProvider } from "@mui/material";
+import { orange } from "@mui/material/colors";
+import axios from "axios";
 
 
 const theme = createTheme({
@@ -10,35 +11,74 @@ const theme = createTheme({
 });
 
 const NewPost = () => {
-  return (
+    const [posts, setPosts] = useState([]);
+    const [postContent, setPostContent] = useState("");
 
-      <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: '100vh' }}
-      >
-          <div>
-              <h2>New Post</h2>
-              <TextField
-                  id="outlined-multiline-static"
-                  label="Enter Blog Post.."
-                  multiline
-                  rows={4}
-                  defaultValue="Default Value"
-              />
-              <br />
-              <ThemeProvider theme={theme}>
-                  <Button variant="contained" sx={{ backgroundColor: theme.status.danger }}>Send</Button>
-              </ThemeProvider>
-          </div>
-      </Grid>
-  );
+    const sendData = (data) => {
+        const postData = {
+            user_id: 10041,
+            id: 456,
+            title: "bitch",
+            body: data,
+            created_at : new Date().toISOString().slice(0, 19).replace('T', ' ')
+
+        };
+
+        axios
+            .post('http://localhost:5000/new-post', postData)
+            .then((res) => {
+                console.log("this is my data");
+                console.log(res.data);
+                console.log("this is my end");
+                setPosts(res.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    // useEffect(() => {
+    //     const getData = () => {
+    //         axios.post('http://localhost:5000/new-post', data).then((res) => {
+    //             console.log(res.data);
+    //             setPosts(res.data);
+    //         });
+    //     };
+    //     getData().then((r) => {});
+    // }, []);
+
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
+        >
+            <div>
+                <h2>New Post</h2>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Enter Blog Post.."
+                    multiline
+                    rows={4}
+                    value={postContent}
+                    onChange={(e) => setPostContent(e.target.value)}
+                />
+                <br />
+                <ThemeProvider theme={theme}>
+                    <Button
+                        variant="contained"
+                        sx={{ backgroundColor: theme.status.danger }}
+                        onClick={() => sendData(postContent)}
+                    >
+                        Send
+                    </Button>
+                </ThemeProvider>
+            </div>
+        </Grid>
+    );
 };
 
 export default NewPost;
-
-
-
