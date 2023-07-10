fa962447103abbbe1a4edc7996d46e1ff3713159
ShayMalichi <46198407+ShayMalichi@users.noreply.github.com>
2023-07-10 23:29:00 +0300

    fd

--- src/CommentPostWindow.js
+++ src/CommentPostWindow.js
@@ -8,7 +8,7 @@ const CommentPostWindow = ({ comments }) => {
             {comments.map((comment) => (
                 <div className="comment-post-window" key={comment.id}>
                     <div className="comment-header">
-                        <span className="username">{comment.username}</span>
+                        <span className="username">{comment.user_id}</span>
                     </div>
                     <p className="content">{comment.body}</p>
                 </div>
