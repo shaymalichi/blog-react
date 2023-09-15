da758695a4616e87b6a0ef70a32977bea9297fb0
ShayMalichi <46198407+ShayMalichi@users.noreply.github.com>
2023-09-15 08:40:59 +0300

    comment ver

--- src/CommentOnPost.js
+++ src/CommentOnPost.js
@@ -4,19 +4,23 @@ import { Button, TextareaAutosize } from '@mui/material';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
-const EditPostComponent = ({isUsername}) => {
+const EditPostComponent = ({ isUsername }) => {
     const { postId } = useParams();
     const navigate = useNavigate();
     const [postContent, setPostContent] = useState('');
 
-    const handleSave = (postContent) => {
-        axios
-            .post('/comments', { postid: postId, content: postContent, user: isUsername})
-            .then(() => {})
-            .catch(error => {
-                console.error(error);
-            });
-        navigate(-1);
+    const handleSave = () => {
+        const isConfirmed = window.confirm('Are you sure you want to save the comment?');
+
+        if (isConfirmed) {
+            axios
+                .post('/comments', { postid: postId, content: postContent, user: isUsername })
+                .then(() => {})
+                .catch(error => {
+                    console.error(error);
+                });
+            navigate(-1);
+        }
     };
 
     const handleCancel = () => {
@@ -29,10 +33,10 @@ const EditPostComponent = ({isUsername}) => {
                 value={postContent}
                 onChange={(e) => setPostContent(e.target.value)}
                 rowsMin={5}
-                placeholder="Enter your post content"
+                placeholder="Enter your comment content"
                 style={{ width: '100%', marginBottom: '16px' }}
             />
-            <Button variant="contained" color="primary" onClick={() => handleSave(postContent)} style={{ marginRight: '8px' }}>
+            <Button variant="contained" color="primary" onClick={handleSave} style={{ marginRight: '8px' }}>
                 Save
             </Button>
             <Button variant="outlined" color="primary" onClick={handleCancel}>
